e845b526f8a0afe0d43d2d48fb73b7c9fb6f325c
unknown <josealvarez1654@outlook.com>
2023-06-15 22:51:26 -0500

    dom2

---
+++ index.js
@@ -0,0 +1,8 @@
+function encendidoB(){
+    document.getElementById("bombillo").setAttribute("src","./img/Encendido.webp");
+}
+
+function apagadoB(){
+    document.getElementById("bombillo").setAttribute("src","./img/apagado.jpg")
+}
+
